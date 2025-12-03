bf24203304cf7a80c7e62073452d484ad9322f6c
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2025-12-03 15:36:03 -0300

    feat: add index.js file to use cases and repositories

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,8 +1,10 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 
-import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
-import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import {
+    PostgresCreateUserRepository,
+    PostgresGetUserByEmailRepository,
+} from '../repositories/postgres/index.js'
 import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserUseCase {
@@ -19,16 +21,20 @@ export class CreateUserUseCase {
             throw new EmailAlreadyInUseError(createUserParams.email)
         }
 
+        // gerar ID do usuário
         const userId = uuidv4()
 
+        // criptografar a senha
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
 
+        // inserir o usuário no banco de dados
         const user = {
             ...createUserParams,
             id: userId,
             password: hashedPassword,
         }
 
+        // chamar o repositório
         const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
         const createdUser = await postgresCreateUserRepository.execute(user)
