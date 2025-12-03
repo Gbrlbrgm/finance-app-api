1cbdcda952a74cb053ad6e14b314d02f796c8bc9
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2025-12-03 14:38:19 -0300

    feat: add UpdateUserUseCase

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -7,7 +7,6 @@ import { EmailAlreadyInUseError } from '../errors/user.js'
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
-        // TODO: verificar se o e-mail já está em uso
         const postgresGetUserByEmailRepository =
             new PostgresGetUserByEmailRepository()
 
@@ -20,20 +19,16 @@ export class CreateUserUseCase {
             throw new EmailAlreadyInUseError(createUserParams.email)
         }
 
-        // gerar ID do usuário
         const userId = uuidv4()
 
-        // criptografar a senha
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
 
-        // inserir o usuário no banco de dados
         const user = {
             ...createUserParams,
             id: userId,
             password: hashedPassword,
         }
 
-        // chamar o repositório
         const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
         const createdUser = await postgresCreateUserRepository.execute(user)
