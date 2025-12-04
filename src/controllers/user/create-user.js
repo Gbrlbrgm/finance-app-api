dd934de8501e5989633a78853ba4144f4bc96cf1
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2025-12-04 16:59:30 -0300

    refactor: add validate required fields helper function

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -7,12 +7,14 @@ import {
     badRequest,
     created,
     serverError,
+    validateRequiredFields,
 } from '../helpers/index.js'
 
 export class CreateUserController {
     constructor(createUserUseCase) {
         this.createUserUseCase = createUserUseCase
     }
+
     async execute(httpRequest) {
         try {
             const params = httpRequest.body
@@ -24,10 +26,13 @@ export class CreateUserController {
                 'password',
             ]
 
-            for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length === 0) {
-                    return badRequest({ message: `Missing param: ${field}` })
-                }
+            const { ok: requiredFieldsWereProvided, missingField } =
+                validateRequiredFields(params, requiredFields)
+
+            if (!requiredFieldsWereProvided) {
+                return badRequest({
+                    message: `The field ${missingField} is required.`,
+                })
             }
 
             const passwordIsValid = checkIfPasswordIsValid(params.password)
